3f26226bb9e95b30b0b4ad93b33b0d04ed58393f
d 4 4 z y <107853484+kanjutzzz@users.noreply.github.com>
2022-06-20 20:43:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ By : ${chalk.bold('Mr.Russel#0001')} - Credit : @dkmpostor & @Eskey ft @xDast#87
       const trophy = data.User.SkillRating;
       const crown = data.User.Crowns;
 
-console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
-      await sleep(150);
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.purple(`User : ${username}`)} | ${chalk.cyan(`Trophy : ${trophy}`)} | ${chalk.green(`Crown : ${crown}`)}`));
+      await sleep(3000);
 
     } else if (result == 'BANNED') {
       console.log(chalk.bgRed(`Your Account has been Banned`));
